032512751365f43c6870d5254ef3710a6355e308
JustinRudnick <rudnick.justin@web.de>
2026-09-15 14:30:24 +0200

    Update neuralgas.go

--- NeuralGas/neuralgas.go
+++ NeuralGas/neuralgas.go
@@ -448,7 +448,10 @@ func (ng *NeuralGas) TrainPlots(epochs, maxCores uint, filenames string, plotEpo
 		}
 
 		if logTPCPE != nil {
-			logTPCPE.WriteString(fmt.Sprintf("%d,%d", epoch+1, adjustedPrototypes))
+			_, err = logTPCPE.WriteString(fmt.Sprintf("%d,%d", epoch+1, adjustedPrototypes))
+			if err != nil && logger != nil {
+				logger.Warn(fmt.Sprintf("could not write to file %s: %s", logFileTrainedPrototypeCoundPerEpoch, err.Error()))
+			}
 		}
 		// end log trained prototype count per epoch
 
